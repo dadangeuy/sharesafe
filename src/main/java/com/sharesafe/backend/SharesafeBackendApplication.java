41508f4df59d9a1b089fbfd1eb9ebabbda382125
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2018-05-30 02:11:22 +0700

    initial project

---
+++ src/main/java/com/sharesafe/backend/SharesafeBackendApplication.java
@@ -0,0 +1,12 @@
+package com.sharesafe.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharesafeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharesafeBackendApplication.class, args);
+    }
+}

b925969572e015a491208fbe87a0af328877d3fe
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2018-05-30 07:36:17 +0700

    add error message

--- src/main/java/com/sharesafe/desktop/controller/DirectoryListingController.java
+++ src/main/java/com/sharesafe/desktop/controller/DirectoryListingController.java
@@ -11,6 +11,7 @@ import io.datafx.controller.flow.action.ActionTrigger;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.stage.FileChooser;
 import org.apache.commons.io.FileUtils;
@@ -18,16 +19,16 @@ import retrofit2.Call;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
-import java.io.IOException;
 import java.security.KeyPair;
-import java.security.spec.InvalidKeySpecException;
 import java.util.List;
 
-@FXMLController("/templates/DirectoryListing.fxml")
+@FXMLController("/templates/DirectoryListingView.fxml")
 public class DirectoryListingController {
+    @FXML @ActionTrigger("connect") Button connectBtn;
     @FXML @ActionTrigger("upload") Button uploadBtn;
     @FXML @ActionTrigger("download") Button downloadBtn;
     @FXML private ListView<String> filesView;
+    @FXML private Label errorView;
 
     private final KeyPair pair = RsaUtil.generatePair();
     private final FileChooser chooser = new FileChooser();
@@ -36,37 +37,61 @@ public class DirectoryListingController {
     private ObservableList<String> files;
 
     @PostConstruct
-    public void init() throws IOException {
+    public void init() {
         files = filesView.getItems();
-        populateFiles();
     }
 
-    private void populateFiles() throws IOException {
-        List<String> newFiles = service.getListFiles().execute().body();
-        files.clear();
-        files.addAll(newFiles);
+    @ActionMethod("connect")
+    public void populateFiles() {
+        hideException();
+        try {
+            List<String> newFiles = service.getListFiles().execute().body();
+            files.clear();
+            files.addAll(newFiles);
+        } catch (Exception e) {
+            showException(e);
+        }
     }
 
     @ActionMethod("upload")
-    public void upload() throws IOException, InvalidKeySpecException {
-        File file = chooser.showOpenDialog(SharesafeDesktopApplication.primaryStage);
-        byte[] fileBytes = FileUtils.readFileToByteArray(file);
-        RsaTransferData data = service.requestKey(new RsaTransferData()).execute().body();
-        data._setData(fileBytes);
-        data.setFilename(file.getName());
-        service.uploadFile(data).execute();
-        populateFiles();
+    public void upload() {
+        hideException();
+        try {
+            File file = chooser.showOpenDialog(SharesafeDesktopApplication.primaryStage);
+            byte[] fileBytes = FileUtils.readFileToByteArray(file);
+            RsaTransferData data = service.requestKey(new RsaTransferData()).execute().body();
+            data._setData(fileBytes);
+            data.setFilename(file.getName());
+            service.uploadFile(data).execute();
+            populateFiles();
+        } catch (Exception e) {
+            showException(e);
+        }
     }
 
     @ActionMethod("download")
-    public void download() throws IOException, InvalidKeySpecException {
-        RsaTransferData request = new RsaTransferData()
-                .setFilename(filesView.getFocusModel().getFocusedItem())
-                ._setPublicKey(pair.getPublic());
-        request._getPublicKey();
-        Call<RsaTransferData> call = service.downloadFile(request);
-        RsaTransferData response = call.execute().body();
-        File file = chooser.showSaveDialog(SharesafeDesktopApplication.primaryStage);
-        FileUtils.writeByteArrayToFile(file, response._getData(pair.getPrivate()));
+    public void download() {
+        hideException();
+        try {
+            RsaTransferData request = new RsaTransferData()
+                    .setFilename(filesView.getFocusModel().getFocusedItem())
+                    ._setPublicKey(pair.getPublic());
+            request._getPublicKey();
+            Call<RsaTransferData> call = service.downloadFile(request);
+            RsaTransferData response = call.execute().body();
+            File file = chooser.showSaveDialog(SharesafeDesktopApplication.primaryStage);
+            FileUtils.writeByteArrayToFile(file, response._getData(pair.getPrivate()));
+        } catch (Exception e) {
+            showException(e);
+        }
+    }
+
+    private void hideException() {
+        errorView.setVisible(false);
+    }
+
+    private void showException(Exception e) {
+        errorView.setVisible(true);
+        errorView.setText(e.getMessage());
     }
 }
